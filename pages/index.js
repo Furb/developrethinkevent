b1db95e96c4ec40aa4bc54dfa4ce1ac75426ce33
Furb <jthabo@gmail.com>
2021-12-20 00:17:08 +0100

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { gql } from "@apollo/client";
+import { client } from "../lib/apollo";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +15,45 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>Rethink Event</h1>
       </main>
 
+      <div className="posts">
+        {posts.map(({ title, uri }) => (
+          <div key={uri}>
+            <a>{title}</a>
+          </div>
+        ))}
+      </div>
+
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p>
+          Rethink Event +45 22 50 77 42 info@rethinkevent.dk CVR-NR: 35124616
+          Vaerksted Karupvej 6 8000 Aarhus C
+        </p>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const result = await client.query({
+    query: gql`
+      query getPosts {
+        posts {
+          nodes {
+            title
+            slug
+            date
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      posts: result.data.posts.nodes,
+    },
+  };
 }
